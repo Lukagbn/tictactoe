9baa47e108bb6cc5a2147d29b4cf0ab0cf4f96b3
lukagbn <luka68690@gmail.com>
2026-07-15 03:17:06 +0400

    feat: add working winning function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.sass";
+
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 function App() {
   const [user, setUser] = useState<boolean>(false);
+  const [choices, setChoices] = useState<number[]>([]);
+  const [botIndex, setBotIndex] = useState<number[]>([]);
+  const [userIndex, setUserIndex] = useState<number[]>([]);
+
   function getIndex(index: number) {
-    console.log(index);
+    if (choices.includes(index)) return;
+    if (!user) {
+      setBotIndex((prev) => [...prev, index]);
+    } else {
+      setUserIndex((prev) => [...prev, index]);
+    }
+    return setChoices((prev) => [...prev, index]);
   }
+  function checkWin(playerIndex: number[]) {
+    return winningCombos.some((combo) =>
+      combo.every((index) => playerIndex.includes(index)),
+    );
+  }
+  useEffect(() => {
+    if (checkWin(botIndex)) {
+      console.log("bot wins");
+    }
+    if (checkWin(userIndex)) {
+      console.log("user wins");
+    }
+    console.log("bot:", botIndex);
+    console.log("user:", userIndex);
+  }, [userIndex, botIndex]);
+
   return (
     <main>
       <h2 className="turn">{user ? "user's" : "bot's"}Turn</h2>
@@ -20,7 +57,7 @@ function App() {
                 setUser(!user);
               }}
             >
-              {index + 1}
+              {choices.includes(index) ? index : null}
             </div>
           );
         })}
